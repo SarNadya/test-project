50503da989ffcfadcfc3df55302e446b564f208b
SarNadya <lutik.ne@gmail.com>
2022-11-14 01:02:39 +0300

    added grid layout

---
+++ src/components/App/App.js
@@ -0,0 +1,128 @@
+import React from "react";
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <div className={styles.item_1}>
+        1
+      </div>
+      <div className={styles.item_2}>
+        2
+      </div>
+      <div className={styles.item_3}>
+        3
+      </div>
+      <div className={styles.item_4}>
+        4
+      </div>
+      <div className={styles.item_5}>
+        5
+      </div>
+      <div className={styles.item_6}>
+        6
+      </div>
+      <div className={styles.item_7}>
+        7
+      </div>
+      <div className={styles.item_8}>
+        8
+      </div>
+      <div className={styles.item_9}>
+        9
+      </div>
+      <div className={styles.item_10}>
+        10
+      </div>
+      <div className={styles.item_11}>
+        11
+      </div>
+      <div className={styles.item_7}>
+        12
+      </div>
+      <div className={styles.item_6}>
+        13
+      </div>
+      <div className={styles.item_7}>
+        14
+      </div>
+      <div className={styles.item_6}>
+        15
+      </div>
+      <div className={styles.item_16}>
+        16
+      </div>
+      <div className={styles.item_7}>
+        17
+      </div>
+      <div className={styles.item_7}>
+        18
+      </div>
+      <div className={styles.item_6}>
+        19
+      </div>
+      <div className={styles.item_7}>
+        20
+      </div>
+      <div className={styles.item_21}>
+        21
+      </div>
+      <div className={styles.item_9}>
+        22
+      </div>
+      <div className={styles.item_23}>
+        23
+      </div>
+      <div className={styles.item_24}>
+        24
+      </div>
+      <div className={styles.item_25}>
+        25
+      </div>
+      <div className={styles.item_26}>
+        26
+      </div>
+      <div className={styles.item_27}>
+        27
+      </div>
+      <div className={styles.item_7}>
+        28
+      </div>
+      <div className={styles.item_6}>
+        29
+      </div>
+      <div className={styles.item_9}>
+        30
+      </div>
+      <div className={styles.item_7}>
+        31
+      </div>
+      <div className={styles.item_6}>
+        32
+      </div>
+      <div className={styles.item_7}>
+        33
+      </div>
+      <div className={styles.item_34}>
+        34
+      </div>
+      <div className={styles.item_35}>
+        35
+      </div>
+      <div className={styles.item_6}>
+        36
+      </div>
+      <div className={styles.item_21}>
+        37
+      </div>
+      <div className={styles.item_7}>
+        38
+      </div>
+      <div className={styles.item_9}>
+        39
+      </div>
+    </div>
+  );
+}
+
+export default App;
